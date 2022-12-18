f4acf4cbfbc2a093f35664c936bcca1b1d7f8557
muhammadanang <muhanangmahrub@gmail.com>
2022-12-18 18:45:01 +0700

    added a login feature

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,22 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    user: {
+      id: '',
+      name: '',
+    }
   },
   getters: {
   },
   mutations: {
+    connect(state, data) {
+      state.user.id = data.id;
+      state.user.name = data.name;
+    },
+    disconnect(state) {
+      state.user.id = '';
+      state.user.name = '';
+    }
   },
   actions: {
   },
